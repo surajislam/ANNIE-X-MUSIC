a21adfa6f1e8adef62341731e2ed0eaf2ce1194f
surajislam <154444093+surajislam@users.noreply.github.com>
2024-08-26 00:01:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ BOT_NAME = getenv("BOT_NAME", "˹HAYA ♪")
 ASSUSERNAME = getenv("ASSUSERNAME", "Hello_babyyii")
 EVALOP = list(map(int, getenv("EVALOP", "6797202080").split()))
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
-LOGGER_ID = int(getenv("LOGGER_ID", --1002126936204))
+LOGGER_ID = int(getenv("LOGGER_ID", -1002035023865))
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 # External APIs - Get these from their respective providers
 GPT_API = getenv("GPT_API")
